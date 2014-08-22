f6129e7054e84c77262711ccfe68771af7ccd323
mobi2010 <zsc@2010.mobi>
2014-08-22 19:12:26 +0800

    init

--- application/views/pinery/public/register_step1.php
+++ application/views/pinery/public/register_step1.php
@@ -3,5 +3,20 @@
 		<span class="register-step1-left">帐号:</span>
 		<span class="register-step1-right"><input type="text" /></span>
 	</div>
-
+	<div>
+		<span class="register-step1-left">帐号:</span>
+		<span class="register-step1-right"><input type="text" /></span>
+	</div>
+	<div>
+		<span class="register-step1-left">帐号:</span>
+		<span class="register-step1-right"><input type="text" /></span>
+	</div>
+	<div>
+		<span class="register-step1-left">帐号:</span>
+		<span class="register-step1-right"><input type="text" /></span>
+	</div>
+	<div>
+		<span class="register-step1-left">帐号:</span>
+		<span class="register-step1-right"><input type="text" /></span>
+	</div>
 </div>
