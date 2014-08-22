611dd400000cbd60e80849ba5f768e2330f63c4a
mobi2010 <zsc@2010.mobi>
2014-08-22 19:37:21 +0800

    init

--- application/views/pinery/public/register_step1.php
+++ application/views/pinery/public/register_step1.php
@@ -1,22 +1,22 @@
-<div class="register-step1">
+<div class="register">
 	<div>
 		<span class="register-step1-left">帐号:</span>
-		<span class="register-step1-right"><input type="text" /></span>
+		<span class="register-step1-right"><input type="text" class="input-text"/></span>
 	</div>
 	<div>
 		<span class="register-step1-left">帐号:</span>
-		<span class="register-step1-right"><input type="text" /></span>
+		<span class="register-step1-right"><input type="text" class="input-text"/></span>
 	</div>
 	<div>
 		<span class="register-step1-left">帐号:</span>
-		<span class="register-step1-right"><input type="text" /></span>
+		<span class="register-step1-right"><input type="text" class="input-text" /></span>
 	</div>
 	<div>
 		<span class="register-step1-left">帐号:</span>
-		<span class="register-step1-right"><input type="text" /></span>
+		<span class="register-step1-right"><input type="text" class="input-text"/></span>
 	</div>
 	<div>
 		<span class="register-step1-left">帐号:</span>
-		<span class="register-step1-right"><input type="text" /></span>
+		<span class="register-step1-right"><input type="text" class="input-text"/></span>
 	</div>
 </div>
