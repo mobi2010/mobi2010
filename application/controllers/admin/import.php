2221bbb1fdc4fde3f73748e9da644f9f46c74c6f
mobi2010 <zsc@2010.mobi>
2014-09-30 12:41:42 +0800

    im

--- application/controllers/admin/import.php
+++ application/controllers/admin/import.php
@@ -35,61 +35,72 @@ class Import extends Admin_Controller {
 
 
 		
-		$html = mobi_gb2u($this->util->curlGet('http://www.zhuozhoufangchan.com/sale/d-158052.html'));
+		$html = mobi_gb2u($this->util->curlGet('http://www.zhuozhoufangchan.com/sale/d-153858.html'));
 
-		//preg_match('/<span class="brokerPhoto">(.*?)<img src="(.*?)"(.*?)<\/span>/is', $html, $matches);	
-
-		// $image = $matches[2];
-		// $dimage = $this->image->wget(array('file'=>'http://www.zhuozhoufangchan.com/upfile/broker/avatar/20140607160107.jpg'));
-		// if($dimage['status'] == 200){
-		// 	$thumbImg = $this->image->thumb(array('file'=>$dimage['data'],'width'=>120,'height'=>120,'bgcolor'=>'black'));
-		// 	$ypyImg = $this->image->ypyUpload(array('file'=>$thumbImg['filePath']));
-		// 	$avatar = $ypyImg['filePath'];
-		// }
+		
 		preg_match('/<div class="detailMainRight">(.*?)<\/div>/is', $html, $matches);
 
 		$infoHtml = $matches[1];
 
-		// preg_match('/<div class="detailMainLeft">(.*?)<\/div>/is', $html, $matches);
-		// $memberHtml = $matches[1];
-							
-		// preg_match('/<li>姓名：(.*?)<\/li>/is', $memberHtml, $matches);	
-		// $names = $matches[1];
+		
+		preg_match('/<div class="detailMainLeft">(.*?)<\/div>/is', $html, $matches);
+		$memberHtml = $matches[1];
 
-		// preg_match('/<li>公司：(.*?)<\/li>/is', $memberHtml, $matches);	
-		// $org_names = $matches[1];
+
+		preg_match('/<span class="brokerPhoto">(.*?)<img src="(.*?)"(.*?)<\/span>/is', $memberHtml, $matches);	
+
+		$image = $matches[2];
+		$dimage = $this->image->wget(array('file'=>'http://www.zhuozhoufangchan.com/upfile/broker/avatar/20140607160107.jpg'));
+		if($dimage['status'] == 200){
+			$thumbImg = $this->image->thumb(array('file'=>$dimage['data'],'width'=>120,'height'=>120,'bgcolor'=>'black'));
+			$ypyImg = $this->image->ypyUpload(array('file'=>$thumbImg['filePath']));
+			$member['avatar'] = mobi_string_filter($ypyImg['filePath']);
+		}
+
+		preg_match('/<li>姓名：(.*?)<\/li>/is', $memberHtml, $matches);	
+		$member['names'] = mobi_string_filter($matches[1]);
+
+		preg_match('/<li>公司：(.*?)<\/li>/is', $memberHtml, $matches);	
+		$member['org_names'] = mobi_string_filter($matches[1]);
 
 		
 
-		// preg_match('/<p class="phoneNumber">看房咨询：<span class="familyAlpha size18px">(.*?)<\/span><\/p>/is', $infoHtml, $matches);
-		// $mobile = $matches[1];
+		preg_match('/<p class="phoneNumber">看房咨询：<span class="familyAlpha size18px">(.*?)<\/span><\/p>/is', $infoHtml, $matches);
+		$member['mobile'] = mobi_string_filter($matches[1]);
 		
 
 
 		preg_match('/<p class="title color690 size14px weightBold">(.*?)<\/p>/is', $infoHtml, $matches);
-		$title = $matches[1];
+		$property['title'] = $matches[1];
 
 		preg_match('/<p class="subTitle size14px weightBold">【(.*?)】&nbsp;(.*?)室(.*?)厅(.*?)卫(.*?)阳&nbsp;\|&nbsp;(.*?)平米<\/p>/is', $infoHtml, $matches);
 		
-		$title = mobi_string_filter($matches[1]);
+		$property['location'] = mobi_string_filter($matches[1]);
 
-		$room = $matches[2];
-		$hall = $matches[3];
-		$bathroom = $matches[4];
-		$area = $matches[6];
+		$property['room'] = $matches[2];
+		$property['hall'] = $matches[3];
+		$property['bathroom'] = $matches[4];
+		$property['area'] = $matches[6];
 
 		preg_match('/<td width="52%">售价：<span class="familyAlpha colorF60 size24px">(.*?)<\/span>/is', $infoHtml, $matches);
 
-		$price = $matches[1];
+		$property['price'] = $matches[1];
 
 		preg_match('/<td>楼层：(.*?)F\/(.*?)F<\/td>/is', $infoHtml, $matches);
-		$floors = $matches[1];
-		$floors_total = $matches[2];
+		$property['floors'] = $matches[1];
+		$property['floors_total'] = $matches[2];
 		preg_match('/<td>朝向：(.*?)<\/td>/is', $infoHtml, $matches);	
-		$toward = $matches[1];			
+		$property['toward'] = $matches[1];			
 		preg_match('/<td>装修：(.*?)<\/td>/is', $infoHtml, $matches);
-		$decoration = $matches[1];
-		var_dump($floors,$floors_total,$toward,$decoration);
+		$property['decoration'] = $matches[1];
+
+		preg_match('/<div class="houseDescription">(.*?)<\/div>/is', $html, $matches);
+		$contentHtml = $matches[1];
+		preg_match_all('/<img(.*?)\/>/is', $contentHtml, $matches);
+		$property['content'] = str_replace($matches[0],'',$contentHtml);
+
+
+		var_dump($member,$property);
 	}
 	/**
 	 * [source description]
